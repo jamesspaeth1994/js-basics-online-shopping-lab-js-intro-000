65e44f9f2e9828f58aab3502e416cb7c8c5604ff
reflective-foobar-0518 <jamesspaeth1994@gmail.com>
2021-06-15 01:15:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,23 +6,71 @@ function getCart() {
 
 function setCart(c) {
   cart = c;
-  return cart;
 }
 
 function addToCart(item) {
- // write your code here
+  // write your code here
+ var price = Math.floor(Math.random() * 101);
+ var item = item;
+ var itemAndPrice = { [item]: price };
+ cart.push(itemAndPrice);
+ console.log(`${item} has been added to your cart.`);
+ return cart;
 }
 
 function viewCart() {
   // write your code here
+  if (cart.length === 0) {
+    console.log("Your shopping cart is empty.")
+  } else {
+    var message = "In your cart, you have ";
+    var item = cart[0];
+    var itemName = Object.keys(item)[0];
+    var itemPrice = item[itemName];
+    message += `${itemName} at $${itemPrice}`;
+    if (cart.length == 2) {
+      var item2 = cart[1];
+      var itemName2 = Object.keys(item2)[0];
+      var itemPrice2 = item2[itemName2];
+      message += ` and ${itemName2} at $${itemPrice2}`;
+    } else if (cart.length >= 3) {
+      for (let i = 1; i < cart.length; i++) {
+        var varItem = cart[i];
+        var varItemName = Object.keys(varItem)[0];
+        var varItemPrice = varItem[varItemName];
+        message += `, ${varItemName} at $${varItemPrice}`;
+      }
+      var lastItem = cart[cart.length - 1];
+      var lastItemName = Object.keys(lastItem)[0];
+      var lastItemPrice = lastItem[lastItemName];
+      message += `, and ${lastItemName} at $${lastItemPrice}`;
+    }
+  message += '.';
+  console.log(message);
+  }
 }
 
 function total() {
   // write your code here
+  var total = 0;
+  for (let i = 0; i < cart.length; i++) {
+    var item = cart[i];
+    var itemName = Object.keys(item)[0];
+    total += item[itemName];
+  }
+  return total;
 }
 
 function removeFromCart(item) {
   // write your code here
+  console.log(item);
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].hasOwnProperty(item)) {
+      cart.splice(i, 1);
+      return cart;
+    }
+  }
+  console.log("That item is not in your cart.");
 }
 
 function placeOrder(cardNumber) {
